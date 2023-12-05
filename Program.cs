1dd640a39c357e993b60ab70b7ff1a4db805e741
Aita Dmit <aita29@mail.ru>
2023-12-05 21:42:35 +0900

    Задания ко второму семинару

--- Program.cs
+++ Program.cs
@@ -1,14 +1,33 @@
-﻿Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿// Console.Write("Введите число: ");// Задача № 3.
+// int number = Convert.ToInt32(Console.ReadLine());
+// int temp = number;
+// if (number >= 100)
+// {
+//    while (number > 999)
+//    {
+//        number /= 10; //убирает последнюю цифру с конца
+//    } 
+//    Console.WriteLine($"{temp} => {number % 10}");
+//    //Console.WriteLine($"Число после очистки: {number}");
+   
+// }
+// else
+// {
+//     Console.WriteLine("Число имеет меньше 3 цифр :)");
+// }
 
-string digits = string.Empty;
-// 45 / 10 -> 4 / 10 -> 0
-while (number > 0)
-{
-    //45 => "5" + " " + " " = "5   "
-    digits = (number % 10).ToString() + "\t" + digits;
-    number /= 10; // number = number / 10
-    // 45/10=4
-}
 
-Console.WriteLine($"Итого: {digits}");
+// Console.Write("Введите число: ");// Задача № 4.
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// string digits = string.Empty;
+// // 45 / 10 -> 4 / 10 -> 0
+// while (number > 0)
+// {
+//     //45 => "5" + " " + " " = "5   "
+//     digits = (number % 10).ToString() + ", " + digits;
+//     number /= 10; // number = number / 10
+//     // 45/10=4
+// }
+
+// Console.WriteLine($"Итого: {digits}");
