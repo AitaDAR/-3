54f7c2f379c1d5fad14761c5596266650536e6e9
Aita Dmit <aita29@mail.ru>
2023-12-05 21:07:24 +0900

    Первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string digits = string.Empty;
+// 45 / 10 -> 4 / 10 -> 0
+while (number > 0)
+{
+    //45 => "5" + " " + " " = "5   "
+    digits = (number % 10).ToString() + "\t" + digits;
+    number /= 10; // number = number / 10
+    // 45/10=4
+}
+
+Console.WriteLine($"Итого: {digits}");
